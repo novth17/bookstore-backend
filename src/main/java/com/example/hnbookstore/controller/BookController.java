8d9ebd33214b38187b9d753efe0acc138b9e72e7
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2025-02-04 20:58:58 +0200

    done add and delete book

--- src/main/java/com/example/hnbookstore/controller/BookController.java
+++ src/main/java/com/example/hnbookstore/controller/BookController.java
@@ -3,6 +3,9 @@ package com.example.hnbookstore.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.hnbookstore.domain.Book;
 import com.example.hnbookstore.domain.BookRepository;
@@ -26,5 +29,23 @@ public class BookController {
         List<Book> books = (List<Book>) bookRepository.findAll();
         model.addAttribute("books", books);
         return "booklist";
+    }
+
+    @GetMapping("/addbook")
+    public String addBookForm() {
+        return "addbook";
+    }
+
+    @PostMapping("/addbook")
+    public String addBook(String title, String author, String publicationYear, String isbn, double price) {
+        Book book = new Book(title, author, publicationYear, isbn, price);
+        bookRepository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteBook(@PathVariable Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/booklist";
     }
 }
