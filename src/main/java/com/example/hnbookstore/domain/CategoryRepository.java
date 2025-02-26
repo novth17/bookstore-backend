f2dd58d3a0bd3275687b04bc291f6910a8263623
Hien Nguyen <hien.nguyen2@myy.haaga-helia.fi>
2025-02-26 22:13:38 +0200

    Exposed CategoryRepository as REST API

--- src/main/java/com/example/hnbookstore/domain/CategoryRepository.java
+++ src/main/java/com/example/hnbookstore/domain/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.example.hnbookstore.domain;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.Optional;
 
+@RepositoryRestResource(path = "categories")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 }
-
